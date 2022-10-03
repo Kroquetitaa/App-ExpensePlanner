8ce835c97cb22dff80ff91c0cdd6fab89cf5ca3e
Kroquetitaa <Kroquetita@gmail.com>
2022-10-04 00:00:04 +0200

    reset data on state

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,7 @@ const App = () => {
           setAnimarModal={setAnimarModal}
           guardarGasto={guardarGasto}
           gastoEditar={gastoEditar}
+          setGastoEditar={setGastoEditar}
         />
       )}
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,6 +8,7 @@ const Modal = ({
   setAnimarModal,
   guardarGasto,
   gastoEditar,
+  setGastoEditar,
 }) => {
   const [mensaje, setMensaje] = useState("");
   const [nombre, setNombre] = useState("");
@@ -29,6 +30,7 @@ const Modal = ({
 
   const ocultarModal = () => {
     setAnimarModal(false);
+    setGastoEditar({});
     setTimeout(() => {
       setModal(false);
     }, 500);
